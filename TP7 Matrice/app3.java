4de2b307d2c810b14a8ca9ff327f61479cf9b050
Chaymahannoun <hannounchayma@gmail.com>
2025-02-26 21:00:22 +0100

    Premier commit de l'application 2

--- TP7 Matrice/app3.java
+++ TP7 Matrice/app3.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class app3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[][] M = new int[20][30];  // La matrice a une taille maximale de 20x30
-        int[] T;  // Tableau pour stocker la somme des lignes
+        int[][] M = new int[20][30];  
+        int[] T;  
         int nl, nc;
 
-        // Demander le nombre de lignes (nl) entre 1 et 20
+       
         do {
             System.out.print("Enter the number of rows (nl, 1-20): ");
             nl = scanner.nextInt();
         } while (nl <= 0 || nl > 20);
 
-        // Demander le nombre de colonnes (nc) entre 1 et 30
+       
         do {
             System.out.print("Enter the number of columns (nc, 1-30): ");
             nc = scanner.nextInt();
         } while (nc <= 0 || nc > 30);
 
-        // Initialiser le tableau T pour stocker la somme des lignes
+       
         T = new int[nl];
 
-        // Demander à l'utilisateur d'entrer les éléments de la matrice
+       
         System.out.println("Enter the elements of the matrix:");
         for (int i = 0; i < nl; i++) {
             for (int j = 0; j < nc; j++) {
@@ -30,16 +30,16 @@ public class app3 {
             }
         }
 
-        // Calculer la somme des éléments pour chaque ligne
+        
         for (int i = 0; i < nl; i++) {
             int sum = 0;
             for (int j = 0; j < nc; j++) {
                 sum += M[i][j];
             }
-            T[i] = sum;  // Stocker la somme de chaque ligne dans T
+            T[i] = sum;  
         }
 
-        // Trouver la somme minimale et maximale
+        
         int minSum = T[0];
         int maxSum = T[0];
         for (int i = 1; i < nl; i++) {
@@ -51,7 +51,7 @@ public class app3 {
             }
         }
 
-        // Afficher les résultats
+        
         System.out.println("Minimum sum of rows: " + minSum);
         System.out.println("Maximum sum of rows: " + maxSum);
 
