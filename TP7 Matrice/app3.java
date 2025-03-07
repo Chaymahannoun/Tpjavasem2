e10c75f08b7a3d0537b1109e690cd0402eefb1a8
Chaymahannoun <hannounchayma@gmail.com>
2025-03-07 12:55:51 +0100

    tp8

--- TP7 Matrice/app3.java
+++ TP7 Matrice/app3.java
@@ -5,41 +5,29 @@ public class app3 {
         Scanner scanner = new Scanner(System.in);
         int[][] M = new int[20][30];  
         int[] T;  
-        int nl, nc;
-
-       
+        int nl, nc;   
         do {
             System.out.print("Enter the number of rows (nl, 1-20): ");
             nl = scanner.nextInt();
-        } while (nl <= 0 || nl > 20);
-
-       
-        do {
+        } while (nl <= 0 || nl > 20);     
+       do {
             System.out.print("Enter the number of columns (nc, 1-30): ");
             nc = scanner.nextInt();
-        } while (nc <= 0 || nc > 30);
-
-       
+        } while (nc <= 0 || nc > 30);     
         T = new int[nl];
-
-       
         System.out.println("Enter the elements of the matrix:");
         for (int i = 0; i < nl; i++) {
             for (int j = 0; j < nc; j++) {
                 M[i][j] = scanner.nextInt();
             }
-        }
-
-        
+        }        
         for (int i = 0; i < nl; i++) {
             int sum = 0;
             for (int j = 0; j < nc; j++) {
                 sum += M[i][j];
             }
             T[i] = sum;  
-        }
-
-        
+        }       
         int minSum = T[0];
         int maxSum = T[0];
         for (int i = 1; i < nl; i++) {
@@ -49,9 +37,7 @@ public class app3 {
             if (T[i] > maxSum) {
                 maxSum = T[i];
             }
-        }
-
-        
+        }       
         System.out.println("Minimum sum of rows: " + minSum);
         System.out.println("Maximum sum of rows: " + maxSum);
 
